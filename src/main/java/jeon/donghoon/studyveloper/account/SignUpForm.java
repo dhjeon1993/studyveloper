82412b61def16947bb5b3ba857f456af1e77a1ef
DONGHOON JEON <dhjeon1993@gmail.com>
2020-06-04 23:01:19 +0900

    회원가입 폼 화면 작성

---
+++ src/main/java/jeon/donghoon/studyveloper/account/SignUpForm.java
@@ -0,0 +1,12 @@
+package jeon.donghoon.studyveloper.account;
+
+import lombok.Data;
+
+@Data
+public class SignUpForm {
+    private String nickname;
+
+    private String password;
+
+    private String email;
+}
