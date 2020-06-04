918dfdbddb94b68e1f7887037bfbe145b0537903
DONGHOON JEON <dhjeon1993@gmail.com>
2020-06-05 00:42:14 +0900

    회원가입 폼 검증

--- src/main/java/jeon/donghoon/studyveloper/account/SignUpForm.java
+++ src/main/java/jeon/donghoon/studyveloper/account/SignUpForm.java
@@ -2,11 +2,23 @@ package jeon.donghoon.studyveloper.account;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 public class SignUpForm {
+
+    @NotBlank
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
     private String nickname;
 
+    @NotBlank
+    @Size(min=3, max = 20)
     private String password;
 
+    @Email
+    @NotBlank
     private String email;
 }
